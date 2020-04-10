7a95d2f485cc2c1c91d7aed953d0e244847f1fef
anujarora0502 <anujarora0502@gmail.com>
2020-04-11 04:19:41 +0530

    initial commit

---
+++ game.js
@@ -0,0 +1,16 @@
+var randomnumber1=Math.ceil(Math.random()*6);
+
+var randomnumber2=Math.ceil(Math.random()*6);
+
+document.querySelector(".img1").setAttribute("src","images/dice"+randomnumber1+".png");
+
+document.querySelector(".img2").setAttribute("src","images/dice"+randomnumber2+".png");
+
+if(randomnumber1>randomnumber2)
+   document.querySelector("h1").innerHTML="🚩Player 1 Wins!";
+
+else if(randomnumber1<randomnumber2)
+   document.querySelector("h1").innerHTML="Player 2 Wins!🚩";
+
+else
+   document.querySelector("h1").innerHTML="Draw!";
